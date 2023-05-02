c2e08f6b40687c8c4dffd63b47855e53b86a0c91
Nick-Phaita <nickphaita@gmail.com>
2023-05-02 19:45:41 +0300

    Corrected hello.php

---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php 
+echo "Hello World";
+echo '<br>' ; 
+function add($firstNumber,$secondNumber){
+    $sum = $firstNumber + $secondNumber;
+    echo "The sum of ".$firstNumber. " and " . $secondNumber . " is " . $sum;
+}
+add(21,30);
+?>
